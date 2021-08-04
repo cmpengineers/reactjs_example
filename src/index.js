e078de9b6b605fb6baaf8d1587f6c185b76bc157
abbas khajah <akhajah@cmp-engineers.com>
2021-08-04 15:20:29 +0300

    how to create property

--- src/index.js
+++ src/index.js
@@ -3,13 +3,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
+
+// function getButtonText() {
+//     return 'click on me xxx';
+// }
+
 // create react component
 const App =  ()  => {
+
+    const buttonText = { text: 'Click me' };
+    const style = { backgroundColor: 'red' , color: 'white' };
+
     return (
     <div>
      <label className="label" for="name" > Enter name </label>
      <input id="name" type="text" />
-        <button style={{ backgroundColor: 'red', color: 'white' }}> Submit</button>
+        {/* <button style={{ backgroundColor: 'red', color: 'white' }}>  { buttonText.text } </button> */}
+        <button style={style}>  { buttonText.text } </button>
     </div>
    );
 };
@@ -21,6 +32,6 @@ ReactDOM.render(
 
 
 
-{/* if (module.hot) {
-    module.hot.accept();
-  } */}
+//   if (module.hot) {
+//          module.hot.accept();
+//    } 
