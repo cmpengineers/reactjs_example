550f4d454dbf8591325df9a3ba932f456c60187c
abbas khajah <akhajah@cmp-engineers.com>
2021-08-04 15:30:24 +0300

    htmlFor from for

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const App =  ()  => {
 
     return (
     <div>
-     <label className="label" for="name" > {labelText} </label>
+     <label className="label" htmlFor="name" > {labelText} </label>
      <input id="name" type="text" />
         {/* <button style={{ backgroundColor: 'red', color: 'white' }}>  { buttonText.text } </button> */}
         <button style={style}>  { buttonText.text } </button>
